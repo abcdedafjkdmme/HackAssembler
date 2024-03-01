984e9dd1845baf9c4bb6f900137c1a34cc81354a
abrka <ifradnashit@gmail.com>
2024-03-01 18:45:01 +0600

    fixed writing to output file doesnt provide pure binary

--- HackAssemblerProgram/main.cpp
+++ HackAssemblerProgram/main.cpp
@@ -6,7 +6,8 @@
 
 int main() {
 	std::filesystem::path input_filepath{ "C:/Program Files/nand2tetris/projects/06/rect/RectL.asm"};
-	std::filesystem::path output_filepath{ "hack_res.hack"};
+	std::filesystem::path output_filepath{ "D:/VsProjs/nand2tetris/rect.hack"};
+	std::filesystem::path output_bin_filepath{ "D:/VsProjs/nand2tetris/rect.bin" };
 
 	std::ifstream input_file{ input_filepath};
 	if (not input_file) {
@@ -14,9 +15,14 @@ int main() {
 		return -1;
 	}
 	std::ofstream output_file{ output_filepath };
-
-	if (not output_file.good()) {
+	if (not output_file) {
 		std::cerr << "couldnt open ouptut file";
+		return -1;
+	}
+
+	std::ofstream output_file_bin{ output_bin_filepath, std::iostream::binary };
+	if (not output_file_bin) {
+		std::cerr << "couldnt open ouptut bin file";
 		return -1;
 	}
 
@@ -40,10 +46,27 @@ int main() {
 		return -1;
 	}
 
-	for (auto bin : assembler_output) {
-		output_file << std::get<bincode>(bin) << "\n";
-	}
+	std::string output_file_bin_buffer{};
 
+	for (auto bin : assembler_output) {
+		const char output_bin_1 = (char)(std::get<bincode>(bin).to_ulong() >> 8);
+		const char output_bin_2 = (char)(std::get<bincode>(bin).to_ulong() & 0b0000000011111111);
+		output_file_bin_buffer.push_back(output_bin_1);
+		output_file_bin_buffer.push_back(output_bin_2);
+
+		output_file << std::get<bincode>(bin) << "\n";
+
+		std::cout << std::hex << std::get<bincode>(bin).to_ulong() << "\n";
+		std::cout << std::bitset<8>(std::get<bincode>(bin).to_ulong() >> 8);
+		std::cout << std::bitset<8>(std::get<bincode>(bin).to_ulong() & 0b0000000011111111);
+
+		std::cout << "\n\n";
+
+	}
+	std::cout << "\n\n";
+
+
+	output_file_bin.write(output_file_bin_buffer.data(), output_file_bin_buffer.size());
 
 	return 0;
 }
